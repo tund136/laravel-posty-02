0e5c6fe054a2645f1e63ad0f631c2407a87d45f9
TuNguyen <55817830+tund136@users.noreply.github.com>
2021-08-25 10:58:15 +0700

    Registing user

---
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class RegisterController extends Controller {
+    public function index() {
+        return view('auth.register');
+    }
+
+    public function store(Request $request) {
+        // Validation
+        $this->validate($request, [
+            'name' => 'required|max:255',
+            'username' => 'required|max:255',
+            'email' => 'required|max:255',
+            'password' => 'required|confirmed'
+        ]);
+
+        // Store user
+        User::create([
+            'name' => $request->name,
+            'username' => $request->username,
+            'email' => $request->email,
+            'password' => Hash::make($request->password)
+        ]);
+
+        // Sign the user in
+
+        // Redirect
+        return redirect()->route('dashboard');
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -20,7 +20,7 @@
     <ul class="flex items-center">
         <li><a href="" class="p-3">Tu Nguyen</a></li>
         <li><a href="" class="p-3">Login</a></li>
-        <li><a href="" class="p-3">Register</a></li>
+        <li><a href="{{route('register')}}" class="p-3">Register</a></li>
         <li><a href="" class="p-3">Logout</a></li>
     </ul>
 </nav>
